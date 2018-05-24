d421a7a77d19a47da586016176e04808f20df4cd
Peerapol <floks.ffv1@hotmail.com>
2018-05-24 15:24:21 +0700

    flokedit2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = [[{"name":"Reeakak","phoneNumber":"0876399982"},{"name":"","phoneNumber":""},{"name":"Papure","phoneNumber":"0889744411"},{"name":"qqqqqq","phoneNumber":"0878855544"}]]
+let contacts = []
 
 app.use(bodyParser.json())
 app.use(cors())
